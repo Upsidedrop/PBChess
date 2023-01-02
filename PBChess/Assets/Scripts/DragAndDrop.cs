0e7ee48aec92305872aa1f8728ebfd69539f58cb
Upside Gaming <danashby2000@gmail.com>
2023-01-02 15:43:24 -0500

    Update Beta.04
    prevented pieces from going off the edge and began preparing for the next update. I'm going to take a break now...

--- PBChess/Assets/Scripts/DragAndDrop.cs
+++ PBChess/Assets/Scripts/DragAndDrop.cs
@@ -5,6 +5,9 @@ using UnityEngine;
 public class DragAndDrop : MonoBehaviour
 {
     Renderer Renderer;
+    [SerializeField]
+    TileManager TileManager;
+    int lastPos;
     private void Awake()
     {
         Renderer = GetComponent<Renderer>();
@@ -18,11 +21,29 @@ public class DragAndDrop : MonoBehaviour
     }
     private void OnMouseUp()
     {
-       
-        print(transform.position.y - 0.58395f);
+        if (Mathf.Abs(transform.position.x) > 4.6716 || Mathf.Abs(transform.position.y) > 4.6716||lastPos == (int)(((Mathf.Round((transform.position.x / 1.1679f) - 0.58395f) * 1.1679f) + 0.58395f) / 1.1679f + 4.5f
+            + (((Mathf.Round(transform.position.y / 1.1679f - 0.58395f) * 1.1679f) + 0.58395f) / 1.1679f + 4.5f) * 8) - 8)
+        {
+            transform.position = new Vector2(
+                (((lastPos % 8) - 5) * 1.1679f) + 0.58395f,
+                ((((lastPos + 8)-(lastPos % 8))/8 - 5) * 1.1679f) + 0.58395f);
+            Renderer.sortingOrder = 1;
+            return;
+        }
+        TileManager.pieces[lastPos-1] = "None";
         transform.position = new Vector2(
-            (Mathf.Round(transform.position.x / 1.1679f - 0.58395f) * 1.1679f)+ 0.58395f,
+             (Mathf.Round(transform.position.x / 1.1679f - 0.58395f) * 1.1679f) + 0.58395f,
             (Mathf.Round(transform.position.y/ 1.1679f - 0.58395f) * 1.1679f)+ 0.58395f);
         Renderer.sortingOrder = 1;
+        print(((transform.position.x / 1.1679f + 4.5f
+            + ((transform.position.y / 1.1679f + 4.5f) * 8) - 8)) - 1);
+        TileManager.pieces[Mathf.RoundToInt(((transform.position.x / 1.1679f + 4.5f
+            + ((transform.position.y / 1.1679f + 4.5f) * 8) - 8))-1)] = gameObject.name;
+    }
+    private void OnMouseDown()
+    {
+        lastPos = ((int)(((Mathf.Round((transform.position.x / 1.1679f) - 0.58395f) * 1.1679f) + 0.58395f) / 1.1679f + 4.5f
+            + (((Mathf.Round(transform.position.y / 1.1679f - 0.58395f) * 1.1679f) + 0.58395f) / 1.1679f + 4.5f) * 8) - 8);
+        print(lastPos);
     }
 }
---
+++ PBChess/Assets/Scripts/TileManager.cs
@@ -0,0 +1,14 @@
+
+using UnityEngine;
+
+public class TileManager : MonoBehaviour
+{
+     public string[] pieces = {"W_Rook","W_Knight","W_Bishop","W_Queen","W_King", "W_Bishop", "W_Knight", "W_Rook", 
+                              "W_Pawn","W_Pawn","W_Pawn","W_Pawn","W_Pawn","W_Pawn","W_Pawn","W_Pawn",
+                             "None","None","None","None","None","None","None","None",
+                             "None","None","None","None","None","None","None","None",
+                             "None","None","None","None","None","None","None","None",
+                             "None","None","None","None","None","None","None","None",
+                             "B_Pawn","B_Pawn","B_Pawn","B_Pawn","B_Pawn","B_Pawn","B_Pawn","B_Pawn",
+                             "B_Rook","B_Knight","B_Bishop","B_Queen","B_King", "B_Bishop", "B_Knight", "B_Rook",};
+}
